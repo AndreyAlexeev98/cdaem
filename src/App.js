3e6eb726287d274bc77596200da4551f2372e42f
Andrey Alexeev <ipalexeev98@mail.ru>
2022-06-08 21:44:06 +0300

    create header-top

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__top container">
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">Главная</li>
+              <li className="nav__item active">Новости</li>
+              <li className="nav__item">Размещение и тарифы</li>
+              <li className="nav__item nav__item--pointer">Объявления на карте</li>
+              <li className="nav__item">Контакты</li>
+            </ul>
+          </nav>
+          <div className='bookmarks'>Закладки</div>
+          <div className='btn-sign-in'>Вход и регистрация</div>
+        </div>
+          <div className="header__bottom">
+            <div className="container">
+              <img src={logo} className="logo" alt="logo" />
+              <ul className="menu">
+                <li className="menu__item menu__item--pointer">Квартиры в Минске</li>
+                <li className="menu__item">Коттеджи и усадьбы</li>
+                <li className="menu__item">Бани и Сауны</li>
+                <li className="menu__item">Авто напрокат</li>
+                <li className="menu__item">Квартиры в Минске</li>
+              </ul>
+              <button type="button" className="btn-add">
+                <span className="btn-add__text">Разместить объявление</span>
+              </button>
+            </div>
+          </div>
       </header>
     </div>
   );
