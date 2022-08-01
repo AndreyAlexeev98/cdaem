2b81ddf7b7dacf6a5b04aed9c155e22773a99340
Andrey Alexeev <ipalexeev98@mail.ru>
2022-08-01 11:32:34 +0300

    fixed remane bug layout

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import './Index.scss';
+import './index.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
